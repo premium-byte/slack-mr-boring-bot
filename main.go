5834f4f075b5b4b66a43a715a2d058743b955f1f
Pedro <pedro87silva@gmail.com>
2024-05-15 23:27:49 +0100

    Add new version that supports multiple teams, and cron messages

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
 	"io"
 	"os"
 	"strings"
-	"time"
+	"sync"
 
 	"github.com/joho/godotenv"
-	"github.com/robfig/cron/v3"
+	"github.com/robfig/cron"
 	"github.com/slack-go/slack"
 )
 
-var originalTeams Configuration
-var selectedUsers Configuration
+var generalconfiguration GeneralConfiguration
+var currentSelectionStorage CurrentSelectionStorage
 var slackApi *slack.Client
 
 func main() {
@@ -24,27 +24,38 @@ func main() {
 	slackToken := os.Getenv("SLACK_TOKEN")
 	slackApi = slack.New(slackToken)
 
-	// Load the configuration from the JSON file
-	originalTeams, _ = loadConfiguration("configuration.json")
-	// Load the selected users from the JSON file
-	selectedUsers, _ = loadConfiguration("selected_users.json")
+	generalconfiguration = loadGeneralConfiguration()
+	currentSelectionStorage = loadCurrentSelectionStorage()
+	defer saveSelectedUsers(currentSelectionStorage)
 
-	defer saveSelectedUsers(selectedUsers)
+	var mu sync.Mutex
+	var wg sync.WaitGroup
 
-	// Initial selection when the program starts
-	selectUser()
+	for teamName, taskMap := range generalconfiguration.Teams {
+		for taskName, task := range taskMap {
+			wg.Add(1)
+			go func(teamName string, taskName string, task Task) {
+				defer wg.Done()
 
-	c := cron.New()
-	// Schedule the function to run daily at 9:00 AM
-	_, err := c.AddFunc("*/1 * * * *", dailyTask)
-	if err != nil {
-		fmt.Println("Error scheduling task:", err)
-		return
+				c := cron.New()
+
+				err := c.AddFunc(task.Cron, func() {
+					mu.Lock()
+					selectUserForTask(teamName, taskName)
+					mu.Unlock()
+				})
+
+				if err != nil {
+					fmt.Println("Error scheduling task:", err)
+					return
+				}
+
+				c.Start()
+			}(teamName, taskName, task)
+		}
 	}
 
-	// Start the scheduler
-	c.Start()
-
+	wg.Wait()
 	// Keep the main function running indefinitely
 	select {}
 }
@@ -62,78 +73,32 @@ type TeamSelected struct {
 	Members []string `json:"members"`
 }
 
-func loadConfiguration(fileName string) (Configuration, error) {
-	fmt.Println("Loading selected users from file...")
-
-	var selectedUsers Configuration
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		// File does not exist or error reading the file, return empty structure
-		return Configuration{Teams: make(map[string][]string)}, nil
-	}
-	defer file.Close()
-
-	data, err := io.ReadAll(file)
-	if err != nil {
-		// Error reading file, return empty structure
-		fmt.Println("Error reading file:", err)
-		return Configuration{Teams: make(map[string][]string)}, nil
-	}
-
-	err = json.Unmarshal(data, &selectedUsers)
-	if err != nil {
-		// Error parsing JSON, return empty structure
-		fmt.Println("Error parsing JSON:", err)
-		return Configuration{Teams: make(map[string][]string)}, nil
-	}
-
-	fmt.Println("Loading selected users from file...FINISHED")
-	return selectedUsers, nil
-}
-
 // saveSelectedUsers saves the selected users to a file
-func saveSelectedUsers(selectedUsers Configuration) {
-	fmt.Println("Saving selected users to file...")
-	data, err := json.MarshalIndent(selectedUsers, "", "  ")
+func saveSelectedUsers(currentSelectionStorage CurrentSelectionStorage) {
+	data, err := json.MarshalIndent(currentSelectionStorage, "", "  ")
 	if err != nil {
 		fmt.Println("Error marshalling selected users:", err)
 		return
 	}
 
-	err = os.WriteFile("selected_users.json", data, 0644)
+	err = os.WriteFile("current_selection_storage.json", data, 0644)
 	if err != nil {
 		fmt.Println("Error writing selected users to file:", err)
 		return
 	}
-	fmt.Println("Saving selected users to file...FINISHED")
 }
 
-func selectUser() {
-	fmt.Println("Selecting user...")
-
-	// Function to select a user
+func selectUserForTask(teamName string, taskName string) {
 	finishedWithoutResults := true
-	for team, members := range originalTeams.Teams {
-		for _, member := range members {
-			if !strings.Contains(strings.Join(selectedUsers.Teams[team], ","), member) {
-				selectedUsers.Teams[team] = append(selectedUsers.Teams[team], member)
-				fmt.Printf("Selected user: %s :: Team %s \n", member, team)
+	for _, member := range generalconfiguration.Teams[teamName][taskName].Members {
+		if !strings.Contains(strings.Join(currentSelectionStorage.Teams[teamName][taskName].Members, ","), member) {
+			finishedWithoutResults = false
+			fmt.Printf("[%s] :: %s selected user %s \n", teamName, taskName, member)
+			addMemberToCurrentSelectionStorage(teamName, taskName, member)
 
-				message := fmt.Sprintf("Hello %s you have been selected to do something!", member)
-				_, _, err := slackApi.PostMessage(
-					"mysuperchannelprivate",
-					slack.MsgOptionText(message, false),
-				)
-				if err != nil {
-					fmt.Printf("Error sending message to Slack: %s\n", err)
-					return
-				}
-
-				saveSelectedUsers(selectedUsers)
-				finishedWithoutResults = false
-				break
-			}
+			taskInfo := generalconfiguration.Teams[teamName][taskName]
+			sendMessageToSlack(taskInfo.Message, member, taskInfo.Channel, taskName)
+			break
 		}
 	}
 
@@ -142,19 +107,132 @@ func selectUser() {
 	}
 
 	fmt.Println("All users have been selected. Resetting...")
-	// Reset selected users
-	selectedUsers.Teams = make(map[string][]string)
-	saveSelectedUsers(selectedUsers)
-	selectUser() // test this better
+	teamTask := currentSelectionStorage.Teams[teamName][taskName]
+	teamTask.Members = []string{}
+	currentSelectionStorage.Teams[teamName][taskName] = teamTask
+	saveSelectedUsers(currentSelectionStorage)
+	//	selectUser() // test this better
 }
 
-func dailyTask() {
-	fmt.Println("Executing daily task at", time.Now())
-	selectUser()
-	// Your code logic here
+func sendMessageToSlack(slackMessage string, member string, channel string, taskName string) {
+	messageToPublish := getMessageToPublish(slackMessage, taskName)
+	messageToPublish = strings.Replace(messageToPublish, "{{name}}", member, -1)
+
+	_, _, err := slackApi.PostMessage(
+		channel,
+		slack.MsgOptionText(messageToPublish, false),
+		slack.MsgOptionAsUser(true),
+	)
+	if err != nil {
+		fmt.Printf("Error sending message to Slack: %s\n", err)
+		return
+	}
+}
+
+func getMessageToPublish(teamTaskMessage string, taskName string) string {
+	if teamTaskMessage != "" {
+		return teamTaskMessage
+	}
+
+	return generalconfiguration.Messages[taskName]
+}
+
+func addMemberToCurrentSelectionStorage(team string, task string, member string) {
+	if _, ok := currentSelectionStorage.Teams[team]; !ok {
+		currentSelectionStorage.Teams[team] = make(map[string]TaskSelection)
+	}
+
+	if _, ok := currentSelectionStorage.Teams[team][task]; !ok {
+		currentSelectionStorage.Teams[team][task] = TaskSelection{
+			Members: []string{},
+		}
+	}
+
+	teamTask := currentSelectionStorage.Teams[team][task]
+	teamTask.Members = append(teamTask.Members, member)
+	// Add member to the existing task
+	currentSelectionStorage.Teams[team][task] = teamTask
+
+	saveSelectedUsers(currentSelectionStorage)
 }
 
 type Configuration struct {
 	Cron  string              `json:"cron"`
 	Teams map[string][]string `json:"teams"`
+}
+
+func loadGeneralConfiguration() GeneralConfiguration {
+
+	var generalConfiguration GeneralConfiguration
+	file, err := os.Open("configuration.json")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		// File does not exist or error reading the file, return empty structure
+		return GeneralConfiguration{Teams: make(map[string]map[string]Task)}
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		// Error reading file, return empty structure
+		fmt.Println("Error reading file:", err)
+		return GeneralConfiguration{Teams: make(map[string]map[string]Task)}
+	}
+
+	err = json.Unmarshal(data, &generalConfiguration)
+	if err != nil {
+		// Error parsing JSON, return empty structure
+		fmt.Println("Error parsing JSON:", err)
+		return GeneralConfiguration{Teams: make(map[string]map[string]Task)}
+	}
+
+	return generalConfiguration
+}
+
+type GeneralConfiguration struct {
+	Messages map[string]string          `json:"messages"`
+	Teams    map[string]map[string]Task `json:"teams"`
+}
+
+type Task struct {
+	Cron    string   `json:"cron"`
+	Members []string `json:"members"`
+	Message string   `json:"message"`
+	Channel string   `json:"channel"`
+}
+
+type CurrentSelectionStorage struct {
+	Teams map[string]map[string]TaskSelection `json:"teams"`
+}
+
+type TaskSelection struct {
+	Members []string `json:"members"`
+}
+
+func loadCurrentSelectionStorage() CurrentSelectionStorage {
+
+	var currentSelectionStorage CurrentSelectionStorage
+	file, err := os.Open("current_selection_storage.json")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		// File does not exist or error reading the file, return empty structure
+		return CurrentSelectionStorage{Teams: make(map[string]map[string]TaskSelection)}
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		// Error reading file, return empty structure
+		fmt.Println("Error reading file:", err)
+		return CurrentSelectionStorage{Teams: make(map[string]map[string]TaskSelection)}
+	}
+
+	err = json.Unmarshal(data, &currentSelectionStorage)
+	if err != nil {
+		// Error parsing JSON, return empty structure
+		fmt.Println("Error parsing JSON:", err)
+		return CurrentSelectionStorage{Teams: make(map[string]map[string]TaskSelection)}
+	}
+
+	return currentSelectionStorage
 }
